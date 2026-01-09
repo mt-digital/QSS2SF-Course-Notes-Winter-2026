e70b6d51dcf156686bb6764a3d7852fad512e2f3
Matt Turner <maturner01@gmail.com>
2026-01-09 06:06:20 -0800

    incorporating renv and scripts in posts

--- posts/identity-coalitions-ergonomics-of-power/R/medici_networks_demo.R
+++ posts/identity-coalitions-ergonomics-of-power/R/medici_networks_demo.R
@@ -41,51 +41,57 @@ g <- graph_from_data_frame(edges, directed = TRUE)
 DEFAULT_ARROW_SIZE = 3
 arrow_lengths <- unit(ifelse(edges$directed, 4, 0), "mm")
 
+
+# 1. run FR once
+lay <- create_layout(g, layout = "fr")
+
+# 2. identify node sets
+obligation_nodes <- edges %>%
+  filter(domain == "OBLIGATION") %>%
+  pull(to) %>%
+  unique()
+
+peer_nodes <- edges %>%
+  filter(domain == "PEERS") %>%
+  pull(to) %>%
+  unique()
+
+# 3. impose vertical structure
+lay <- lay %>%
+  mutate(
+    # pin Medici
+    x = ifelse(name == "Medici", 0, x),
+    y = ifelse(name == "Medici", 2.5, y),
+
+    # obligation nodes below
+    y = ifelse(name %in% obligation_nodes, y - 2.5, y),
+
+    # peers roughly level with Medici
+    y = ifelse(name %in% peer_nodes, y * 0.2 + 1.2, y)
+  )
+
+
 # MT version
 cap_size = 1.25
 p <- 
-  ggraph(g, layout = "fr") +
+  # ggraph(g, layout = "fr") +
+  ggraph(lay) +
   # geom_edge_link(
   geom_edge_fan(
     aes(color = domain), arrow = arrow(length = arrow_lengths),
     start_cap = circle(cap_size, "mm"),
     end_cap = circle(cap_size, "mm"),
     width = 1.25,
-    strength = 2,
+    strength = 3,
     alpha = 0.8) +
   geom_node_point(size = 3) +
-  geom_node_text(aes(label = name), repel = TRUE, size = 7) +
+  geom_node_text(
+    aes(label = name),
+    size = 7,
+    vjust = -1,
+    hjust = 0.5
+  ) +
   scale_edge_color_manual(values = edge_colors, name="Domain") +
   theme_void()
     
 print(p)
-# # ---- asymmetric plot -------------------------------------------------------
-
-# p_asymm <-
-#   ggraph(g_asymm, layout = "fr") +
-#   geom_edge_link(
-#     aes(color = domain),
-#     arrow = arrow(length = unit(2, "mm")),
-#     end_cap = circle(2, "mm"),
-#     alpha = 0.85
-#   ) +
-#   geom_node_point(size = 3) +
-#   geom_node_text(aes(label = name), repel = TRUE, size = 3) +
-#   scale_edge_color_manual(values = edge_colors) +
-#   ggtitle("Asymmetric Power Relationships") +
-#   theme_void()
-
-# # ---- symmetric plot --------------------------------------------------------
-
-# p_symm <-
-#   ggraph(g_symm, layout = "fr") +
-#   geom_edge_link(
-#     aes(color = domain),
-#     alpha = 0.6
-#   ) +
-#   geom_node_point(size = 3) +
-#   geom_node_text(aes(label = name), repel = TRUE, size = 3) +
-#   scale_edge_color_manual(values = edge_colors) +
-#   ggtitle("Symmetric Relationships") +
-#   theme_void()
-
