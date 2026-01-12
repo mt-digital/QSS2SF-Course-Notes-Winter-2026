fec174d1fe21a11497d0df2609618fe5dc7aa77e
Matthew Turner <maturner01@gmail.com>
2026-01-11 16:53:08 -0800

    close to finishing Medici network diagram

--- posts/identity-coalitions-ergonomics-of-power/R/medici_networks.R
+++ posts/identity-coalitions-ergonomics-of-power/R/medici_networks.R
@@ -8,7 +8,7 @@ library(ggplot2)
 get_medici_network <- function(medici_network_csv = "data/medici_network.csv") {
   
   # ---- load data -------------------------------------------------------------
-  edges <- read_csv(
+  edge_tbl <- read_csv(
     medici_network_csv,
     comment = "#",
     col_types = cols(
@@ -19,7 +19,7 @@ get_medici_network <- function(medici_network_csv = "data/medici_network.csv") {
   )
   
   # ---- classify --------------------------------------------------------------
-  edges <- edges %>%
+  edge_tbl <- edge_tbl %>%
     mutate(
       # case_when lets us assign different values for the column on the lhs
       domain = case_when(
@@ -33,30 +33,54 @@ get_medici_network <- function(medici_network_csv = "data/medici_network.csv") {
       directed = domain %in% c("OBLIGATION", "MARRIAGE"),
     )
   
-  g <- graph_from_data_frame(edges, directed = TRUE)
-  print(g)
-  print(edges)
-  return (list(edges = edges, graph = g))
+  g <- graph_from_data_frame(edge_tbl, directed = TRUE)
+
+  return (list(edge_table = edge_tbl, graph = g))
 }
 
 plot_medici_network <- function(medici_network_csv = "data/medici_network.csv",
-                                line_width = 1.25, arrow_size = 3, cap_size = 2,
+                                line_width = 1.25, arrow_size = 2, cap_size = 2,
+                                x_scale = 1.0, y_scale = 1.0,
+                                layout_csv = "data/layout.csv",
                                 edge_colors = c(
                                   OBLIGATION = "#d73027",
                                   MARRIAGE   = "#1a9850",
                                   PEER       = "#7b3294"
                                 )) {
+  
   # Load the Medici edge dataframe and graph
   medici_network_pkg <- get_medici_network(medici_network_csv)
-  print(names(medici_network_pkg))
   # Extract edges
-  edges <- medici_network_pkg$edges
+  edges <- medici_network_pkg$edge_table
   # Extract graph
   g <- medici_network_pkg$graph
   
   ## PLOTTING
-  # 1. Run Fruchterman-Reingold to get initial positions.
-  lay <- create_layout(g, layout = "fr")
+  # 1. Load positions and create layout 
+  # First read the file to a tbl 
+  coords <- read_csv(
+    layout_csv,
+    col_types = cols(
+      name = col_character(),
+      x    = col_double(),
+      y    = col_double()
+    )
+  )
+
+  # Assign vertices x and y positions
+  V(g)$x <- x_scale * coords$x[match(V(g)$name, coords$name)]
+  V(g)$y <- y_scale * coords$y[match(V(g)$name, coords$name)]
+  
+  stopifnot(!any(is.na(V(g)$x)), !any(is.na(V(g)$y)))
+
+  lay <- create_layout(
+    g,
+    layout = "manual",
+    x = V(g)$x,
+    y = V(g)$y
+  )
+
+  # Create layout
   
   # 2. Identify different node sets.
   peer_nodes <- edges %>%
@@ -72,55 +96,81 @@ plot_medici_network <- function(medici_network_csv = "data/medici_network.csv",
     pull(to) %>%
     unique()
   
-  # 3. Impose some structure for different node types.
-  lay <- lay %>%
-    mutate(
-      
-      # obligation nodes below
-      y = ifelse(name %in% obligation_nodes, y - 0.5, y),
-      
-      # marriage nodes get slightly blown out randomly left or right to make room
-      x = ifelse(name %in% marriage_nodes, x + 1*rnorm(1), x),
-      
-      # pin Medici above all
-      x = ifelse(name == "Medici", mean(x), x),
-      y = ifelse(name == "Medici", max(y) + 2, y),
-      
-      # peers roughly level with Medici
-      y = ifelse(name %in% peer_nodes, max(y) - 0.2, y*rnorm(1,1,0.1))
-    )
-  
-  # Build unit-list that  understands, adding arrows for directed edges
+  # 3. Build unit-list that  understands, adding arrows for directed edges
   arrow_lengths <- unit(ifelse(edges$directed, arrow_size, 0), "mm")
   
+  # 4. Do the ploting
   p <- 
-    # Start by plotting the layout computed above
     ggraph(lay) +
-    # Draw edges—"fan" refers to the fact that edges arc for readability
-    geom_edge_fan(
-      aes(color = domain), arrow = arrow(length = arrow_lengths),
-      start_cap = circle(cap_size, "mm"),
-      end_cap = circle(cap_size, "mm"),
-      width = line_width,
-      strength = 3,
-      alpha = 0.8) +
-    # Customize edge color legend
-    scale_edge_color_manual(values = edge_colors, name = "Domain") +
-    # Draw nodes
-    geom_node_point(size = 3) +
-    # Print node labels
-    geom_node_text(
-      aes(label = name),
-      size = 7,
-      repel = TRUE,
-      vjust = -1,
-      hjust = 0.5,
-      
-    ) +
-    # Remove axes or any other plot marks beyond what's spec'd above
-    theme_void()
+      geom_edge_fan(
+        aes(color = domain),
+        arrow = arrow(length = arrow_lengths, type="closed"),
+        start_cap = rectangle(8, 4, "mm"),
+        end_cap   = rectangle(8, 4, "mm"),
+        # start_cap = circle(1, "mm"),
+        # end_cap   = circle(5, "mm"),
+        width = 0.7,
+        alpha = 0.9,
+        strength = 1.15
+      ) +
+      geom_node_label(
+        aes(label = name),
+        size = 3,
+        # label.size = 0.25,
+        label.size = 0.0,
+        fill = "white",
+        alpha = 0.5,
+        # nudge_x = 0.1,
+        # nudge_y = 0.1
+      ) +
+      scale_edge_color_manual(values = edge_colors, name = "Domain") +
+      coord_equal() +
+      theme_void()
+
+#     # Start by plotting the layout computed above
+#     ggraph(lay) +
+
+#     geom_edge_fan(
+#       aes(color = domain),
+#       arrow = arrow(length = unit(3, "mm")),
+#       start_cap = circle(2.5, "mm"),
+#       end_cap   = rectangle(8, 4, "mm"),
+#       width = 1.2,
+#       alpha = 0.8
+#     ) +
+    
+
+#     # Customize edge color legend
+#     scale_edge_color_manual(values = edge_colors, name = "Domain") +
+
+#     # Draw nodes
+#     # geom_node_point(size = 3) +
+
+#     # Print node labels
+#     geom_node_label(
+#       aes(label = name),
+#       size = 5,
+#       repel = TRUE,
+#       # vjust = -1,
+#       # hjust = 0.5
+#     ) +
+#     # geom_node_text(
+#     #   aes(label = name),
+#     #   size = 7,
+#     #   repel = TRUE,
+#     #   vjust = -1,
+#     #   hjust = 0.5
+#     # ) +
+
+#     # Try to keep ggplot from overriding geometry customization
+#     coord_equal() +
+
+#     # Remove axes or any other plot marks beyond what's spec'd above
+#     theme_void()
       
   return (p)
 }
 
-print(plot_medici_network(medici_network_csv = "posts/identity-coalitions-ergonomics-of-power/data/medici_network.csv"))
+
+# plot_medici_network()
+# plot_medici_network(medici_network_csv = "posts/identity-coalitions-ergonomics-of-power/data/medici_network.csv")
